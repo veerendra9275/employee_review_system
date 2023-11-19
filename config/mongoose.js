e34cb5ab411c7c0cfaf2a63477b66c9f626d0e81
veerendra <veerendra.patel9275@gmail.com>
2023-11-19 19:13:49 +0530

    inintial commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 
 // connect from mongodb
 // mongoose.connect('mongodb://localhost:27017/Review-System');
-mongoose.connect('mongodb://localhost:27017/employee_review', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://0.0.0.0:27017/employee_review', {useNewUrlParser: true, useUnifiedTopology: true});
 
 // aquire connection if it is succesful
 const db = mongoose.connection;
 
 // error
-db.on('error', console.error.bind(console, 'error connecting to db'));
+db.on('error', console.error.bind(console, 'er0ror connecting to db'));
 
 // up and running then print the message
 db.once('open', function(){
